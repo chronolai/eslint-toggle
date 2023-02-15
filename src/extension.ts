103b14e83c7bdb41e3b167881f0ede6d08346c3c
chrono.lai <chrono.lai@cycarrier.com>
2023-02-15 10:23:51 +0800

    add main code

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,27 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  let disposable = vscode.commands.registerCommand("eslint-toggle.toggle", () => {
+    const editor = vscode.window.activeTextEditor;
+    if (!editor) {
+      return;
+    }
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "eslint-toggle" is now active!');
+    const document = editor.document;
+    const firstLine = document.lineAt(0);
+    const text = firstLine.text;
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('eslint-toggle.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from eslint-toggle!');
-	});
-
-	context.subscriptions.push(disposable);
+    if (text.startsWith("/* eslint-disable */")) {
+      editor.edit((editBuilder) => {
+        editBuilder.delete(firstLine.rangeIncludingLineBreak);
+      });
+    } else {
+      editor.edit((editBuilder) => {
+        editBuilder.insert(new vscode.Position(0, 0), "/* eslint-disable */\n");
+      });
+    }
+  });
+  context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
